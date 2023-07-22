2c38e18704eebc26b2506a354db023d8b22c9950
Xichen Zhou <xeechou@gmail.com>
2023-07-21 20:46:25 -0400

    triangle with sdl2

---
+++ readme.md
@@ -0,0 +1,4 @@
+# D3D12 Sample
+Building a D3D12 sample project using SDL2 and glm
+
+
---
+++ src/helpers.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// include the basic windows header files and the Direct3D header files
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <dxgi1_4.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <winuser.h>
+#include <wrl/client.h>
+
+#include <vector>
